42d98ec6964724d830ef15f4b132a61ad648b07d
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-04-25 09:28:29 +0000

    Connect App with server

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for, request , redirect
+import os
 from flask_bootstrap import Bootstrap
+import inference
 app = Flask(__name__)
 Bootstrap(app)
 """
 Routes
 """
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = inference.get_prediction(image_path)
+            print('class name =', class_name)
     return render_template('index.html')
 if __name__ == '__main__' :
     app.run(debug = True)
