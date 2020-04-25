15d26a36651683cd632feb73460573b1a7902779
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-04-25 06:57:08 +0000

    Add Bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from flask_bootstrap import Booststrap
 app = Flask(__name__)
-
+Booststrap(app)
 """
 Routes
 """
 @app.route('/', methods = ['GET'])
 def index():
-    return "Hello World"
+    return rennder_template('index.html')
 if __name__ == '__main__' :
     app.run(debug = True)
